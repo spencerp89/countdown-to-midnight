1b220a384d20d8e6ecf77123313426344ff452d8
spencerp89 <spencerp89@ymail.com>
2020-10-06 00:56:00 -0400

    Done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(seconds_left)
+    until seconds_left < 1
+        puts "#{seconds_left} SECOND(S)!"
+        seconds_left -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(seconds_left)
+    until seconds_left < 1
+        puts "#{seconds_left} SECOND(S)!"
+        seconds_left -= 1
+        sleep 1
+    end
+    puts "HAPPY NEW YEAR!"
 end
